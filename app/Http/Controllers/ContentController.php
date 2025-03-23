084453c3e479f3e6d5f509c49c170c033fa81f07
karthi <karthigai.selvan@thedevengine.com>
2025-03-24 00:45:13 +0530

    New Record Insert loginc added in update api

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -13,14 +13,14 @@ class ContentController extends Controller
     {
         $pageId = 1;
         $content = Section::where('nav_id', $pageId)->with('sectionContent')->get();
-        foreach($content as $singleSection) {
-            foreach($singleSection->sectionContent as $singleContent) {
-                $singleContent->image1_path = $singleContent->image1_path ? asset($singleContent->image1_path): $singleContent->image1_path;
-                $singleContent->image2_path = $singleContent->image2_path ? asset($singleContent->image2_path): $singleContent->image2_path;
-                $singleContent->image3_path = $singleContent->image3_path ? asset($singleContent->image3_path): $singleContent->image3_path;
-                $singleContent->image4_path = $singleContent->image4_path ? asset($singleContent->image4_path): $singleContent->image4_path;
-                $singleContent->image5_path = $singleContent->image5_path ? asset($singleContent->image5_path): $singleContent->image5_path;
-                $singleContent->image6_path = $singleContent->image6_path ? asset($singleContent->image6_path): $singleContent->image6_path;
+        foreach ($content as $singleSection) {
+            foreach ($singleSection->sectionContent as $singleContent) {
+                $singleContent->image1_path = $singleContent->image1_path ? asset($singleContent->image1_path) : $singleContent->image1_path;
+                $singleContent->image2_path = $singleContent->image2_path ? asset($singleContent->image2_path) : $singleContent->image2_path;
+                $singleContent->image3_path = $singleContent->image3_path ? asset($singleContent->image3_path) : $singleContent->image3_path;
+                $singleContent->image4_path = $singleContent->image4_path ? asset($singleContent->image4_path) : $singleContent->image4_path;
+                $singleContent->image5_path = $singleContent->image5_path ? asset($singleContent->image5_path) : $singleContent->image5_path;
+                $singleContent->image6_path = $singleContent->image6_path ? asset($singleContent->image6_path) : $singleContent->image6_path;
             }
         }
         return $content;
@@ -100,8 +100,9 @@ class ContentController extends Controller
         return $content;
     }
 
-    public function updateContent(Request $request){
-       
+    public function updateContent(Request $request)
+    {
+
         // dd($request->all());
 
         // $validatedData = $request->validate([
@@ -122,77 +123,133 @@ class ContentController extends Controller
         //     'image6_path' => 'nullable|image', // Ensure it's validated as an image
         //     'image6_name' => 'nullable|string',
         // ]);
-            //  dd($request->all());
-        $content =Content::find($request->id);
-        // $content->section_id = $request->section_id;
-        $title  = $request->title ? $request->title : $content->title;
-        $content = $request->content ? $request->content : $content->content;
-        $content->title = $title;
-        $content->content = $content;
-        // dd($request->hasFile('image1_path'));
-        if ($request->hasFile('image1_path')) {
-            $file = $request->file('image1_path');
-            $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
-            $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
-            $content->image1_path = 'storage/' . $path; // Store relative path in DB
-            $content->image1_name = $request->image1_name ? $request->image1_name : $content->image1_name;
-        }
-        
-        if ($request->hasFile('image2_path')) {
-            $file = $request->file('image2_path');
-            $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
-            $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
-            $content->image2_path = 'storage/' . $path; // Store relative path in DB
-            $content->image2_name = $request->image2_name ? $request->image2_name : $content->image2_name;
+        //  dd($request->all());
+        $content = Content::find($request->id);
+        if (!$content) {
+            $content = new Content;
+            // $content->section_id = $request->section_id;
+            $title  = $request->title ? $request->title : null;
+            $content = $request->content ? $request->content : null;
+            $content->title = $title;
+            $content->content = $content;
+            // dd($request->hasFile('image1_path'));
+            if ($request->hasFile('image1_path')) {
+                $file = $request->file('image1_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image1_path = 'storage/' . $path; // Store relative path in DB
+                $content->image1_name = $request->image1_name ? $request->image1_name : null;
+            }
+
+            if ($request->hasFile('image2_path')) {
+                $file = $request->file('image2_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image2_path = 'storage/' . $path; // Store relative path in DB
+                $content->image2_name = $request->image2_name ? $request->image2_name : null;
+            }
+
+            if ($request->hasFile('image3_path')) {
+                $file = $request->file('image3_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image3_path = 'storage/' . $path; // Store relative path in DB
+                $content->image3_name = $request->image3_name ? $request->image3_name : null;
+            }
+
+            if ($request->hasFile('image4_path')) {
+                $file = $request->file('image4_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image4_path = 'storage/' . $path; // Store relative path in DB
+                $content->image4_name = $request->image4_name ? $request->image4_name : null;
+            }
+
+            if ($request->hasFile('image5_path')) {
+                $file = $request->file('image5_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image5_path = 'storage/' . $path; // Store relative path in DB
+                $content->image5_name = $request->image5_name ? $request->image5_name : null;
+            }
+
+            if ($request->hasFile('image6_path')) {
+                $file = $request->file('image6_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image6_path = 'storage/' . $path; // Store relative path in DB
+                $content->image6_name = $request->image6_name ? $request->image6_name : null;
+            }
+        } else {
+            $title  = $request->title ? $request->title : $content->title;
+            $content = $request->content ? $request->content : $content->content;
+            $content->title = $title;
+            $content->content = $content;
+            // dd($request->hasFile('image1_path'));
+            if ($request->hasFile('image1_path')) {
+                $file = $request->file('image1_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image1_path = 'storage/' . $path; // Store relative path in DB
+                $content->image1_name = $request->image1_name ? $request->image1_name : $content->image1_name;
+            }
+
+            if ($request->hasFile('image2_path')) {
+                $file = $request->file('image2_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image2_path = 'storage/' . $path; // Store relative path in DB
+                $content->image2_name = $request->image2_name ? $request->image2_name : $content->image2_name;
+            }
+
+            if ($request->hasFile('image3_path')) {
+                $file = $request->file('image3_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image3_path = 'storage/' . $path; // Store relative path in DB
+                $content->image3_name = $request->image3_name ? $request->image3_name : $content->image3_name;
+            }
+
+            if ($request->hasFile('image4_path')) {
+                $file = $request->file('image4_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image4_path = 'storage/' . $path; // Store relative path in DB
+                $content->image4_name = $request->image4_name ? $request->image4_name : $content->image4_name;
+            }
+
+            if ($request->hasFile('image5_path')) {
+                $file = $request->file('image5_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image5_path = 'storage/' . $path; // Store relative path in DB
+                $content->image5_name = $request->image5_name ? $request->image5_name : $content->image5_name;
+            }
+
+            if ($request->hasFile('image6_path')) {
+                $file = $request->file('image6_path');
+                $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
+                $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
+                $content->image6_path = 'storage/' . $path; // Store relative path in DB
+                $content->image6_name = $request->image6_name ? $request->image6_name : $content->image6_name;
+            }
         }
 
-        if ($request->hasFile('image3_path')) {
-            $file = $request->file('image3_path');
-            $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
-            $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
-            $content->image3_path = 'storage/' . $path; // Store relative path in DB
-            $content->image3_name = $request->image3_name ? $request->image3_name : $content->image3_name;
-        }
-        
-        if ($request->hasFile('image4_path')) {
-            $file = $request->file('image4_path');
-            $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
-            $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
-            $content->image4_path = 'storage/' . $path; // Store relative path in DB
-            $content->image4_name = $request->image4_name ? $request->image4_name : $content->image4_name;
-        }
-        
-        if ($request->hasFile('image5_path')) {
-            $file = $request->file('image5_path');
-            $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
-            $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
-            $content->image5_path = 'storage/' . $path; // Store relative path in DB
-            $content->image5_name = $request->image5_name ? $request->image5_name : $content->image5_name;
-        }
-        
-        if ($request->hasFile('image6_path')) {
-            $file = $request->file('image6_path');
-            $filename = time() . '_' . $file->getClientOriginalName(); // Unique filename
-            $path = $file->storeAs('uploads/images', $filename, 'public'); // Store in storage/app/public/uploads/images
-            $content->image6_path = 'storage/' . $path; // Store relative path in DB
-            $content->image6_name = $request->image6_name ? $request->image6_name : $content->image6_name;
-        }
-       
+
         $content->save();
 
         $section = Section::find($content->section_id);
         $contents = Section::where('nav_id',  $section->nav_id)->with('sectionContent')->get();
-        foreach($contents as $singleSection) {
-            foreach($singleSection->sectionContent as $singleContent) {
-                $singleContent->image1_path = $singleContent->image1_path ? asset($singleContent->image1_path): $singleContent->image1_path;
-                $singleContent->image2_path = $singleContent->image2_path ? asset($singleContent->image2_path): $singleContent->image2_path;
-                $singleContent->image3_path = $singleContent->image3_path ? asset($singleContent->image3_path): $singleContent->image3_path;
-                $singleContent->image4_path = $singleContent->image4_path ? asset($singleContent->image4_path): $singleContent->image4_path;
-                $singleContent->image5_path = $singleContent->image5_path ? asset($singleContent->image5_path): $singleContent->image5_path;
-                $singleContent->image6_path = $singleContent->image6_path ? asset($singleContent->image6_path): $singleContent->image6_path;
+        foreach ($contents as $singleSection) {
+            foreach ($singleSection->sectionContent as $singleContent) {
+                $singleContent->image1_path = $singleContent->image1_path ? asset($singleContent->image1_path) : $singleContent->image1_path;
+                $singleContent->image2_path = $singleContent->image2_path ? asset($singleContent->image2_path) : $singleContent->image2_path;
+                $singleContent->image3_path = $singleContent->image3_path ? asset($singleContent->image3_path) : $singleContent->image3_path;
+                $singleContent->image4_path = $singleContent->image4_path ? asset($singleContent->image4_path) : $singleContent->image4_path;
+                $singleContent->image5_path = $singleContent->image5_path ? asset($singleContent->image5_path) : $singleContent->image5_path;
+                $singleContent->image6_path = $singleContent->image6_path ? asset($singleContent->image6_path) : $singleContent->image6_path;
             }
         }
         return $contents;
-
     }
 }
